836e395be7e32379e3b8627297c4d92ac15ad297
Nevio Carcangiu <nevio@carcangiu.net>
2025-05-26 15:22:27 +0200

    Lukas Version2 getestet

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-from ev3dev2.sensor.lego import ColorSensor
-from ev3dev2.sensor import INPUT_1
- 
-sensor = ColorSensor(INPUT_1)
- 
-farbe = sensor.color
- 
-if farbe == 1:
-    print("Farbe erkannt: Schwarz")
-elif farbe == 2:
-    print("Farbe erkannt: Blau")
-elif farbe == 3:
-    print("Farbe erkannt: Grün")
-elif farbe == 4:
-    print("Farbe erkannt: Gelb")
-elif farbe == 5:
-    print("Farbe erkannt: Rot")
-elif farbe == 6:
-    print("Farbe erkannt: Weiß")
-elif farbe == 7:
-    print("Farbe erkannt: Braun")
-else:
-    print("Keine Farbe erkannt")
+from pybricks.parameters import Port, Color
+from pybricks.ev3devices import ColorSensor
+color_sensor = ColorSensor(Port.S1)
+
+
+def read_color(colorsensor: ColorSensor) -> str:
+    # Get the detected color from the sensor
+    # color = color_sensor.color()
+
+    # Return the name of the color as a string
+    if color == Color.RED:
+        return "red"
+    elif color == Color.GREEN:
+        return "green"
+    elif color == Color.BLUE:
+        return "blue"
+    elif color == Color.YELLOW:
+        return "yellow"
+    elif color == Color.WHITE:
+        return "white"
+    elif color == Color.BLACK:
+        return "black"
+    elif color == Color.BROWN:
+        return "brown"
+    else:
+        return "unknown"
+
+if read_color(color_sensor) == 'red':
+    print('the red color has been detected')
