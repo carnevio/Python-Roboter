f2f3377a2f1e84017f38ecf5ad9cf404a9574043
LukasTraut <lukastraut@hotmail.com>
2025-06-02 14:37:50 +0200

    hoi

--- color.py
+++ color.py
@@ -1,31 +1,39 @@
-
-
 from pybricks.parameters import Port, Color
 from pybricks.ev3devices import ColorSensor
+
+# Initialize the color sensor
 color_sensor = ColorSensor(Port.S1)
 
+def read_color(color_sensor: ColorSensor) -> str:
+    """
+    Reads the color detected by the color sensor and returns the color name as a string.
 
-def read_color(colorsensor: ColorSensor) -> str:
+    Args:
+        color_sensor (ColorSensor): The color sensor instance.
+
+    Returns:
+        str: The name of the detected color.
+    """
     # Get the detected color from the sensor
-    # color = color_sensor.color()
+    color = color_sensor.color()
+
+    # Dictionary mapping color constants to their string names
+    color_mapping = {
+        Color.RED: "red",
+        Color.GREEN: "green",
+        Color.BLUE: "blue",
+        Color.YELLOW: "yellow",
+        Color.WHITE: "white",
+        Color.BLACK: "black",
+        Color.BROWN: "brown"
+    }
 
     # Return the name of the color as a string
-    if color == Color.RED:
-        return "red"
-    elif color == Color.GREEN:
-        return "green"
-    elif color == Color.BLUE:
-        return "blue"
-    elif color == Color.YELLOW:
-        return "yellow"
-    elif color == Color.WHITE:
-        return "white"
-    elif color == Color.BLACK:
-        return "black"
-    elif color == Color.BROWN:
-        return "brown"
-    else:
-        return "unknown"
+    return color_mapping.get(color, "unknown")
 
-if read_color(color_sensor) == 'red':
-    print('the red color has been detected')
+# Example usage
+detected_color = read_color(color_sensor)
+if detected_color == 'red':
+    print('The red color has been detected')
+else:
+    print(f'The detected color is {detected_color}')
