9b8c73e443a2ef84016fe0f2c1efbe814e336e62
Nevio Carcangiu <nevio@carcangiu.net>
2025-06-09 14:00:57 +0200

    Abgabe version

--- main.py
+++ main.py
@@ -1,50 +1,52 @@
 #!/usr/bin/env pybricks-micropython
+#Rafaeal, Sven, Lukas, Robin, Nevio
 
+
+#All diese Bibliotheken werden geladen
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor
 from pybricks.parameters import Port, Stop, Color
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
 
-# Initialisierung
+
 ev3 = EV3Brick()
 
-# Motoren
-medium_motor = Motor(Port.A)  # Greifarm (Hebel)
+#Diese Ports werden für die Motoren und den Farbsensor verwendet
+medium_motor = Motor(Port.A)  
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
-# Farbsensor vorne
+
 color_sensor = ColorSensor(Port.S4)
 
-# Motor-Reset
+
 medium_motor.reset_angle(0)
 left_motor.reset_angle(0)
 right_motor.reset_angle(0)
 
-# DriveBase einrichten
-wheel_diameter = 56  # mm
-axle_track = 114     # mm
+
+wheel_diameter = 56  
+axle_track = 114     
 drive_base = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
 
-# Farben definieren
+
 OBJECT_COLOR = Color.GREEN
 TARGET_COLOR = Color.BLUE
 
-# Start
+
 ev3.speaker.say("Suche Objekt")
 
-# 1. 10 cm rückwärts
+
 drive_base.straight(-100)
 
-# 2. 15° nach rechts
+
 drive_base.turn(20)
 
-# 3. Farbe prüfen
 if color_sensor.color() == OBJECT_COLOR:
     ev3.speaker.say("Objekt erkannt")
 
-    # 4. Greifarm leicht absenken
+
     medium_motor.run_target(
         speed=200,
         target_angle=-56,
@@ -52,10 +54,10 @@ if color_sensor.color() == OBJECT_COLOR:
         wait=True
     )
 
-    # 5. 15 cm vorwärts
+
     drive_base.straight(100)
 
-    # 6. Greifarm weiter absenken auf 30°
+
     medium_motor.run_target(
         speed=200,
         target_angle=-30,
@@ -63,7 +65,6 @@ if color_sensor.color() == OBJECT_COLOR:
         wait=True
     )
 
-    # 7. Zielzone finden
     ev3.speaker.say("Suche Zielzone")
     while True:
         if color_sensor.color() == TARGET_COLOR:
@@ -73,7 +74,7 @@ if color_sensor.color() == OBJECT_COLOR:
         drive_base.straight(20)
         wait(100)
 
-    # 8. Objekt ablegen (Greifarm wieder hoch)
+    
     medium_motor.run_target(
         speed=200,
         target_angle=60,
@@ -85,4 +86,11 @@ if color_sensor.color() == OBJECT_COLOR:
 
 else:
     ev3.speaker.say("Falsches Objekt")
-    drive_base.straight(100)  # Zurückfahren
+    drive_base.straight(100)  
+
+
+
+#Dieser Code steuert einen EV3 Roboter, der ein Objekt erkennt , dies aufliest, es zu einer Zielzone transportiert und bei der Zielsone wieder absetz.
+
+#Wir haben KI gebraucht um das Projekt in 5 Features zu unterteilen damit jeder ein Feature machen konnte. Zusätzlich haben wir vereinzeilt KI Tools benutzt um den Code zu schreiben, aber den grössten Teil anhand der beispiel Codes von Moodle
+
